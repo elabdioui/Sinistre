f1182323372dcf180cbc9d511dee4c9e4a56a16b
elfiwalid <walid11elfilali@gmail.com>
2025-03-23 00:22:07 +0000

    walid

---
+++ service-senistre/src/test/java/com/pfa/service_senistre/ServiceSenistreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.service_senistre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSenistreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
