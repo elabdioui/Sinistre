6eebbba2899948d13da9caaabd866303b38a1e80
elabdioui <elabdiouihaitam@gmail.com>
2025-07-05 17:23:38 +0100

    configuration eureka

--- service-admin/src/main/java/com/pfa/service_admin/ServiceAdminApplication.java
+++ service-admin/src/main/java/com/pfa/service_admin/ServiceAdminApplication.java
@@ -3,10 +3,13 @@ package com.pfa.service_admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ServiceAdminApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceAdminApplication.class, args);
 	}
 
--- service-authentification/src/main/resources/ServiceSinistreApplication.java
+++ service-authentification/src/main/resources/ServiceSinistreApplication.java
@@ -1,13 +1,13 @@
-package com.pfa.service_senistre;
+package com.pfa.service_sinistre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceSenistreApplication {
+public class ServiceSinistreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceSenistreApplication.class, args);
+		SpringApplication.run(ServiceSinistreApplication.class, args);
 	}
 
 }
